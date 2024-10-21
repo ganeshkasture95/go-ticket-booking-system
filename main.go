a45a22598a538e7fe83d8593baea0be2da363336
Ganesh Kasture <ganeshkasture9595@gmail.com>
2024-10-21 13:31:49 +0530

    pinters

--- main.go
+++ main.go
@@ -4,21 +4,31 @@ import "fmt"
 
 func main() {
 
-	var confranceName = "SG Turf"
+	myname := "Ganesh"
+
+	var confranceName = "GK Merta"
 	const conferenceTicket = 50
 	var availabelTickets uint = 50
 
-	availabelTickets = -1
-
-	fmt.Println("Welcome To", confranceName, "Booking Application")
+	fmt.Println("Welcome To", confranceName, "Booking Application by", myname)
 	fmt.Println("Total Tickets Available:", availabelTickets)
 	fmt.Println("Ticket Price: $", conferenceTicket)
 
 	var userName string
+	fmt.Scan(&userName)
+	fmt.Println("Hello", userName)
 
-	var userTicket = 2
-	userName = "tom"
+	fmt.Println("do you want to book the ticket 1=yes 2=no :")
+	var yesno int
+	fmt.Scan(&yesno)
 
-	fmt.Println("Hello", userName, "You have booked", userTicket, "tickets")
+	if yesno == 1 {
+		fmt.Println("You have booked the ticket")
+		availabelTickets--
+	} else {
+		fmt.Println("Thankyou for visiting the GK Mata")
+	}
+
+	fmt.Println("Hello", userName, "You have booked 1 tickets")
 
 }
