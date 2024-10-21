09882698f73a4d9f6b3344ce5750d2f957eea4b0
Ganesh Kasture <ganeshkasture9595@gmail.com>
2024-10-21 13:14:16 +0530

    data types in go

--- main.go
+++ main.go
@@ -6,13 +6,19 @@ func main() {
 
 	var confranceName = "SG Turf"
 	const conferenceTicket = 50
-	var availabelTickets = 50
+	var availabelTickets uint = 50
+
+	availabelTickets = -1
 
 	fmt.Println("Welcome To", confranceName, "Booking Application")
 	fmt.Println("Total Tickets Available:", availabelTickets)
-
-	// Print the conference ticket
 	fmt.Println("Ticket Price: $", conferenceTicket)
 
-	fmt.Print(conferenceTicket)
+	var userName string
+
+	var userTicket = 2
+	userName = "tom"
+
+	fmt.Println("Hello", userName, "You have booked", userTicket, "tickets")
+
 }
